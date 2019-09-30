82c8e9c5595ef24e57928021bcc1eb54f93415c1
Emeshe Sotak <48476041+szotakemese@users.noreply.github.com>
2019-09-30 18:24:55 +0200

    Add files via upload

--- src/api/index.js
+++ src/api/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 
 const aboutRouter = require("./about");
 const randomRouter = require("./random");
-const task1Router = require("./task1")
+const task1Router = require("./task1");
+const task7Router = require("./task7");
 
 const apiRouter = express.Router();
 apiRouter.use('/about', aboutRouter);
 apiRouter.use('/random', randomRouter);
 apiRouter.use('/task1', task1Router);
+apiRouter.use('/task7', task7Router);
 
 module.exports = apiRouter;
